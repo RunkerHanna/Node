43d51cffe74593978fe93a47e8e0fc9a2f284ae9
RunkerHanna <rkhe@stu.xidian.edu.cn>
2017-05-08 12:16:21 +0800

    update server using event loop

--- requestHandler.js
+++ requestHandler.js
@@ -1,16 +1,43 @@
+/*
 function sleep(milliSec){
     var startTime = new Date().getTime();
     while(new Date().getTime() < startTime + milliSec);
 }
+*/
 
-function start(){
+//var exec = require("child_process").exec;
+function start(response){
     console.log("request 'start' is handled");
-    sleep(10000);
-    return "hello start!";
+    /*
+    exec('dir', {timeout: 10000, maxBuffer: 20000*1024}, function(error, stdout, stderr){
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(stdout);
+        response.end();
+    })
+    */
+    
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
 }
-function upload(){
+function upload(response){
     console.log("request 'upload' is handled");
-    return "hello upload!";
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("hello upload");
+    response.end();
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -3,12 +3,9 @@ var url = require('url');
 function start(route, handle){
     http.createServer(function(request, response){
         var pathname = url.parse(request.url).pathname;
-        console.log('requested from ' + pathname);
+        console.log('requested from ' + pathname + 'received!');
 
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        var content = route(handle, pathname);
-        response.write(content);
-        response.end();
+        route(handle, pathname, response);
     }).listen(8888);
     console.log("server started!");
 }
